05c7b323ae9fe8a5a89d571e567731fbbb62e4c2
pflofif <bogdanvivchar8@gmail.com>
2022-11-02 16:01:18 +0200

    logging is successfully work

--- CompanyEmplayees/Controllers/WeatherForecastController.cs
+++ CompanyEmplayees/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmplayees.Controllers
@@ -6,28 +7,22 @@ namespace CompanyEmplayees.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private readonly ILoggerManager _logger;
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILoggerManager logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            _logger.LogInfo("Here is LogInfo from WeatherForecast");
+            _logger.LogDebug("Here is LogDebug from WeatherForecast");
+            _logger.LogError("Here is LogError from WeatherForecast");
+            _logger.LogWarn("Here is LogWarn from WeatherForecast");
+
+            return new string[] { "value1, value2" };
         }
     }
 }
---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+using NLog;
+
+namespace LoggerService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public LoggerManager()
+        {
+        }
+
+        public void LogInfo(string message)
+        {
+            Logger.Info(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            Logger.Warn(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            Logger.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            Logger.Error(message);
+        }
+    }
+}
